867fc74ea58625f1e609e7ed552e285e2e72e7f7
laura <laurabejan06@gmail.com>
2018-11-17 15:36:01 +0200

    exercitiu

---
+++ src/Ascii.java
@@ -0,0 +1,10 @@
+public class Ascii {
+
+    public static void main(String[] args) {
+
+        for (char i = 0; i < 255; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
--- src/Star.java
+++ src/Star.java
@@ -1,6 +1,26 @@
 public class Star {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        char star = '*';
+        int patratele=80;
+        int speed=100;
 
+        while (true) {
+            for (int c = 0; c < patratele; c++) {
+                printStar(star, speed, c);
+            }
+            for (int c = patratele; c >= 0; c--) {
+                printStar(star, speed, c);
+            }
+        }
+    }
+
+    private static void printStar(char star, int speed, int c) throws InterruptedException {
+        System.out.print("\r");
+        for (int i = 0; i < c; i++) {
+            System.out.print(" ");
+        }
+        System.out.print(star);
+        Thread.sleep(speed);
     }
 }
